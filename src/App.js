9de1ed85bccab5e70647380de5d465a2db9b5b04
Hugo Alves <hugo.alves@hp.com>
2023-08-08 07:53:28 -0300

    Adding og tags

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
